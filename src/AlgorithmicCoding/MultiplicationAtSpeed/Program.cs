7e41d54404f867db51ad790ea5c2a8e04ce79b70
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-08-28 18:24:21 +0200

    Task: Multiplication at speed. (#1)
    * Add blank solution.
    
    * Add new task project.
    
    * Multiply using parallel for loop.
    
    * Add testing.

---
+++ src/AlgorithmicCoding/MultiplicationAtSpeed/Program.cs
@@ -0,0 +1,30 @@
+﻿using MultiplicationAtSpeed;
+using System.Diagnostics;
+
+int size = 100000000;
+var firstArray = new double[size];
+var secondArray = new double[size];
+
+Console.WriteLine($"Generation started at: {DateTime.Now}");
+
+Parallel.ForEach(firstArray, item =>
+{
+    Random rnd = new Random();
+    item = rnd.NextDouble();
+});
+
+Parallel.ForEach(secondArray, item =>
+{
+    Random rnd = new Random();
+    item = rnd.NextDouble();
+});
+
+Console.WriteLine($"Done at: {DateTime.Now}");
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var result = ToyMath.Multiplication_at_Speed(firstArray, secondArray);
+
+stopwatch.Stop();
+Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
---
+++ src/AlgorithmicCoding/MultiplicationAtSpeed/ToyMath.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace MultiplicationAtSpeed;
+
+public static class ToyMath
+{
+    public static double[] Multiplication_at_Speed(double[] firstArray, double[] secondArray)
+    {
+        if (firstArray.Length != secondArray.Length)
+        {
+            throw new ArgumentException("The sizes of the arrays aren't the same");
+        }
+
+        var size = firstArray.Length;
+        var multiplyResult = new double[size];
+
+        Parallel.For(0, size, index =>
+        {
+            multiplyResult[index] = firstArray[index] * secondArray[index];
+        });
+
+        return multiplyResult;
+    }
+}
