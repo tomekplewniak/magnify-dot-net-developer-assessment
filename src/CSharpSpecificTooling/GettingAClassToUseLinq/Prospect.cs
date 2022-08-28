d145a07773a063632dc4d4a1add76dedb4403373
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-28 20:21:35 +0200

    Resolve task.

---
+++ src/CSharpSpecificTooling/GettingAClassToUseLinq/Prospect.cs
@@ -0,0 +1,14 @@
+﻿namespace GettingAClassToUseLinq;
+
+public class Prospect
+{
+    public Prospect(string name, Guid prospectID)
+    {
+        Name = name;
+        ProspectID = prospectID;
+    }
+
+    public string Name { get; set; }
+
+    public Guid ProspectID { get; set; }
+}
---
+++ src/CSharpSpecificTooling/GettingAClassToUseLinq/WeeklyTargets.cs
@@ -0,0 +1,15 @@
+﻿namespace GettingAClassToUseLinq;
+
+public class WeeklyTargets
+{
+    public SortedList<int, Prospect> Prospects { get; set; }
+
+    public double Method1() 
+    { 
+        throw new NotImplementedException();
+    }
+    public double Method2()
+    {
+        throw new NotImplementedException();
+    }
+}
