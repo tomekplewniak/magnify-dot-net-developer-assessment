edb0b8fd1eb676baa999c0bec3f176d6db6947c7
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-28 20:48:41 +0200

    Finish round task.

---
+++ src/CSharpSpecificTooling/BasicPassingAMethodIn/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Basic Passing a Method In");
+
+var random = new Random();
+var firstNumber = random.NextDouble();
+var secodnNumber = random.NextDouble();
+var decimalPlace = 3;
+
+
+double firstResult = 0;
+double secondResult = 0;
+
+Round(firstNumber, secodnNumber,
+    decimalPlace, 
+    ref firstResult, 
+    ref secondResult);
+
+Console.WriteLine($"First result: {firstResult}");
+Console.WriteLine($"Second result: {secondResult}");
+
+
+static void Round(double firstNumb,
+    double secondNum,
+    int decimalPlace,
+    ref double firstResult,
+    ref double secondResult)
+{
+    firstResult = BaseRound(firstNumb, decimalPlace);
+    secondResult = BaseRound(secondNum, decimalPlace);
+}
+
+
+static double BaseRound(double numberNeedsRounding, int decimalPlace)
+{
+    return Math.Round(numberNeedsRounding, decimalPlace);
+}
