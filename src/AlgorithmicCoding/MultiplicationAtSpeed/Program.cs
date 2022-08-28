bc326227956eb57a0988fa12f53f1b8792cac0e0
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-28 18:22:13 +0200

    Add testing.

--- src/AlgorithmicCoding/MultiplicationAtSpeed/Program.cs
+++ src/AlgorithmicCoding/MultiplicationAtSpeed/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MultiplicationAtSpeed;
+using System.Diagnostics;
+
+int size = 100000000;
+var firstArray = new double[size];
+var secondArray = new double[size];
+
+Console.WriteLine($"Generation started at: {DateTime.Now}");
+
+Parallel.ForEach(firstArray, item =>
+{
+    Random rnd = new Random();
+    item = rnd.NextDouble();
+});
+
+Parallel.ForEach(secondArray, item =>
+{
+    Random rnd = new Random();
+    item = rnd.NextDouble();
+});
+
+Console.WriteLine($"Done at: {DateTime.Now}");
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var result = ToyMath.Multiplication_at_Speed(firstArray, secondArray);
+
+stopwatch.Stop();
+Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
