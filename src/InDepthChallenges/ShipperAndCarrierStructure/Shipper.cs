ad5bb29a07045a45e71e7dbf1490f42fe781c72b
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-28 21:21:47 +0200

    Add Shipper.

---
+++ src/InDepthChallenges/ShipperAndCarrierStructure/Shipper.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipperAndCarrierStructure;
+
+public class Shipper
+{
+    public long Id { get; set; }
+
+    public Shipment Shipment { get; set; }
+
+    public void CreateShipments(Shipment shipment)
+    {
+        Shipment = shipment;
+    }
+}
